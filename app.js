2221bb152dc54d9ef333e36e5cc43395fdbd34f6
Nilkamal Shah <nilkamalsha75@gmail.com>
2019-05-03 19:36:35 +0530

    Adding port

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const cors = require("cors");
 app.use(cors());
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
-app.listen(4000, () => {
-  console.log(`Application started to listen on ${4000}`);
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Application started to listen on ${process.env.PORT || 4000}`);
 });
